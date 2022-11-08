2983f04868ca4d34dcd6e64837d2dc95426a0ca2
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2022-11-08 11:49:42 +0000

    contact us

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2dd4c5026ed7628b"; }
+/******/ 		__webpack_require__.h = function() { return "f12dd559bda1f4a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c8f1e52b3788e440.hot-update.js
+++ .next/static/webpack/webpack.c8f1e52b3788e440.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6b001b8d3764a2b1"; }
+/******/ 	__webpack_require__.h = function() { return "028582c6a9b639b6"; }
 /******/ }();
 /******/ 
 /******/ }
