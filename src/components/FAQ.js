e702dbeedf647686932105a95124c4dc20d85582
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-01-05 07:51:46 +0000

    FAQ copy

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -57,6 +57,27 @@ function FAQ() {
               </div>
             </WellernAccordion>
           </Accordion>
+          <Accordion
+            className="faq-accordion pt-10 "
+            id="course-faq"
+            defaultActiveKey=""
+          >
+         
+            <WellernAccordion
+              eventName={"collapse1"}
+              title={"Whats the difference between the Fullstack and Freelance course?"}
+              active={active}
+              onClick={() => onClick("collapse1")}
+            >
+              <div className="course-video-list">
+                <p className='p-40'>
+                Our Fullstack Web Developer course gives you the skills and confidence to start applying to Junior Fullstack roles in the industry by the end of the course. However, if you're not looking to switch careers and just want to earn a side income, the Freelance course is a great option. We teach you necessary skills to find your own clients doing low-code web development and SEO.
+
+                </p>
+               
+              </div>
+            </WellernAccordion>
+          </Accordion>
 		  <Accordion
             className="faq-accordion pt-10 "
             id="course-faq"
@@ -122,64 +143,9 @@ LEARN MORE
               </div>
             </WellernAccordion>
           </Accordion>
-		  <Accordion
-            className="faq-accordion pt-10 "
-            id="course-faq"
-            defaultActiveKey=""
-          >
-         
-            <WellernAccordion
-              eventName={"collapse1"}
-              title={"I don't have a degree, is this still for me?"}
-              active={active}
-              onClick={() => onClick("collapse1")}
-            >
-              <div className="course-video-list">
-                <p className='p-40'>
-				Why would you let that stop you? coding bootcamps gives you the skills which are highly in demand. Since 2020, 90% of graduates from bootcamps are hired!
-                </p>
-               
-              </div>
-            </WellernAccordion>
-          </Accordion>	  <Accordion
-            className="faq-accordion pt-10 "
-            id="course-faq"
-            defaultActiveKey=""
-          >
-         
-            <WellernAccordion
-              eventName={"collapse1"}
-              title={"I don't have a degree, is this still for me?"}
-              active={active}
-              onClick={() => onClick("collapse1")}
-            >
-              <div className="course-video-list">
-                <p className='p-40'>
-				Why would you let that stop you? coding bootcamps gives you the skills which are highly in demand. Since 2020, 90% of graduates from bootcamps are hired!
-                </p>
-               
-              </div>
-            </WellernAccordion>
-          </Accordion>	  <Accordion
-            className="faq-accordion pt-10 "
-            id="course-faq"
-            defaultActiveKey=""
-          >
-         
-            <WellernAccordion
-              eventName={"collapse1"}
-              title={"I don't have a degree, is this still for me?"}
-              active={active}
-              onClick={() => onClick("collapse1")}
-            >
-              <div className="course-video-list">
-                <p className='p-40'>
-				Why would you let that stop you? coding bootcamps gives you the skills which are highly in demand. Since 2020, 90% of graduates from bootcamps are hired!
-                </p>
-               
-              </div>
-            </WellernAccordion>
-          </Accordion>
+        
+          
+		
 		</div>
 	);
 }
