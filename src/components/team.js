b13a3e77272f8e4e3dc29867f18fdc5bacc0fc95
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-01-19 18:57:01 +0000

    adding Jamie to course details page

--- src/components/team.js
+++ src/components/team.js
@@ -1,55 +1,73 @@
-import React from 'react'
+import React from "react";
 
 function team() {
   return (
     <section className="team text-center py-5">
-    <div className="container">
-      <div className="header my-5">
-        <h2 className="py-50">Meet our Team </h2>
-    
+      <div className="container">
+        <div className="header my-5">
+          <h2 className="py-50">Meet our Team </h2>
+        </div>
+        <div className="row">
+          <div className="col-md-6 col-lg-3">
+            <div className="img-block mb-5">
+              <img
+                style={{ maxHeight: "250px" }}
+                src="assets/images/abdulkadir.jpeg"
+                className="img-fluid  img-thumbnail"
+                alt="image1"
+              />
+              <div className="content mt-2">
+                <h4>Abdulkadir M</h4>
+                <p className="text-muted">Fullstack Instructor</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 ">
+            <div class="img-block mb-5">
+              <img
+                style={{ maxHeight: "250px" }}
+                src="assets/images/haroon-pic.jpeg"
+                class="img-fluid  img-thumbnail "
+                alt="image1"
+              />
+              <div class="content mt-2">
+                <h4>Harun S</h4>
+                <p class="text-muted">Web Development Mentor</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="img-block mb-5">
+              <img
+                style={{ maxHeight: "250px" }}
+                src="assets/images/jamie-pic.jpeg"
+                class="img-fluid  img-thumbnail"
+                alt="image1"
+              />
+              <div class="content mt-2">
+                <h4>Jamie H</h4>
+                <p class="text-muted">Web Development Instructor</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="img-block mb-5">
+              <img
+                style={{ maxHeight: "250px" }}
+                src="assets/images/shuayb.jpg"
+                class="img-fluid  img-thumbnail "
+                alt="image1"
+              />
+              <div class="content mt-2">
+                <h4>Shuayb A</h4>
+                <p class="text-muted">Freelance Instructor</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="row">
-        <div className="col-md-6 col-lg-3">
-          <div className="img-block mb-5">
-            <img style={{maxHeight:"250px"}} src="assets/images/abdulkadir.jpeg" className="img-fluid  img-thumbnail" alt="image1"/>
-            <div className="content mt-2">
-              <h4>Abdulkadir M</h4>
-              <p className="text-muted">Fullstack Instructor</p>
-           
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 ">
-          <div class="img-block mb-5">
-            <img style={{maxHeight:"250px"}} src="assets/images/haroon-pic.jpeg" class="img-fluid  img-thumbnail " alt="image1"/>
-            <div class="content mt-2">
-              <h4>Harun S</h4>
-              <p class="text-muted">Web Development Mentor</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="img-block mb-5">
-            <img style={{maxHeight:"250px"}} src="assets/images/jamie-pic.jpeg" class="img-fluid  img-thumbnail" alt="image1"/>
-            <div class="content mt-2">
-              <h4>Jamie H</h4>
-              <p class="text-muted">Web Development Instructor</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-          <div class="img-block mb-5">
-            <img style={{maxHeight:"250px"}} src="assets/images/shuayb.jpg" class="img-fluid  img-thumbnail " alt="image1"/>
-            <div class="content mt-2">
-              <h4>Shuayb A</h4>
-              <p class="text-muted">Freelance Instructor</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default team
+export default team;
