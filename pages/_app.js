b86dcd023d652be3835ed660efe44b970039b91a
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-02-13 17:44:15 +0000

    new GTM script

--- pages/_app.js
+++ pages/_app.js
@@ -17,21 +17,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-       
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PW46K3N}')
-      `,
-        }}
-      />
-
-      <Script
         id="google-analytics"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default function Document() {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     <Html>
       <Head>
-        <noscript
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+        />
+        <script
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PW46K3N" height="0" width="0" style="display: none; visibility: hidden;" />`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GTM_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
           }}
         />
       </Head>
