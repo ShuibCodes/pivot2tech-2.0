f2c076b451d337a4d4d28cb0e97b5a5fbc6a2822
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-06-21 14:57:36 +0100

    copy, spaces update

--- pages/course-details-freelance.js
+++ pages/course-details-freelance.js
@@ -83,7 +83,7 @@ const CourseDetails = () => {
                     marginLeft: "15px",
                   }}
                 >
-                  7 spaces left!
+                  4 spaces left!
                 </h6>
 
                 <div className="image mb-35">
--- pages/course-details.js
+++ pages/course-details.js
@@ -80,7 +80,7 @@ const CourseDetails = () => {
                       marginLeft: "15px",
                     }}
                   >
-                    5 spaces left!
+                    4 spaces left!
                   </h6>
                 </div>
                 <h2>Fullstack Web Development Bootcamp</h2>
