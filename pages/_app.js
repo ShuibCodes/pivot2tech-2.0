1cdfc9ad657873182a5e9c477e96057949f6e4d1
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-07-20 17:44:51 +0100

    fb pixel

--- pages/_app.js
+++ pages/_app.js
@@ -65,9 +65,7 @@ function MyApp({ Component, pageProps }) {
 
       <noscript>
         <img
-          height="1"
-          width="1"
-          style="display:none"
+          style={{ display: "none" }}
           src="https://www.facebook.com/tr?id=605381381461929&ev=PageView&noscript=1"
         />
       </noscript>
