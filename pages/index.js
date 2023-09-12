3fc19ae7fbb06c645bd1d115a848277a36486b50
Shuib Abdillahi <abdullahshuib124@gmail.com>
2023-09-12 17:03:00 +0400

    spaces

--- pages/index.js
+++ pages/index.js
@@ -294,7 +294,7 @@ const Index = () => {
 
                     <div className="d-flex justify-content-around ">
                       <p style={{ fontSize: "18px", color: "blue" }}>
-                        <span>25 spaces left</span>
+                        <span>24 spaces left</span>
                       </p>
                       <div>
                         {" "}
@@ -395,7 +395,7 @@ const Index = () => {
                     </div>
                     <div className="d-flex justify-content-around ">
                       <p style={{ fontSize: "18px", color: "blue" }}>
-                        <span>29 spaces left</span>
+                        <span>21 spaces left</span>
                       </p>
                       <div>
                         {" "}
